8de17a2a1fef0aa333d122604031b861405fb793
Justin Tavares <justintavares@gmail.com>
2022-10-17 03:12:16 -0400

    smarter link structure

---
+++ assets/take_a_look_at_this.js
@@ -0,0 +1,412 @@
+module.exports = {
+  images: [
+    {
+      "link": "https://i.imgur.com/EGXgV6h.png",
+      "weight": 500
+    },
+    {
+      "link": "https://i.imgur.com/HfyA085.png",
+      "weight": 100
+    },
+    {
+      "link": "https://i.imgur.com/Skjnetn.png",
+      "weight": 100
+    },
+    {
+      "link": "https://i.imgur.com/Os3S0E8.png",
+      "weight": 90
+    },
+    {
+      "link": "https://i.imgur.com/HU7iXgs.png",
+      "weight": 90
+    },
+    {
+      "link": "https://i.imgur.com/uBptkLt.png",
+      "weight": 80
+    },
+    {
+      "link": "https://i.imgur.com/VZ9IOOw.png",
+      "weight": 60
+    },
+    {
+      "link": "https://i.imgur.com/HfyA085.png",
+      "weight": 60
+    },
+    {
+      "link": "https://i.imgur.com/OKDcbbF.jpg",
+      "weight": 40
+    },
+    {
+      "link": "https://i.imgur.com/wTukiLQ.png",
+      "weight": 100
+    },
+    {
+      "link": "https://i.imgur.com/A6vpR9I.png",
+      "weight": 100
+    },
+    {
+      "link": "https://i.imgur.com/SEqav03.png",
+      "weight": 100
+    },
+    {
+      "link": "https://i.imgur.com/P04iEYX.png",
+      "weight": 50
+    },
+    {
+      "link": "https://i.imgur.com/pLYT6IQ.jpg",
+      "weight": 50
+    },
+    {
+      "link": "https://i.imgur.com/erOEdPI.png",
+      "weight": 30
+    },
+    {
+      "link": "https://i.imgur.com/S803l9H.png",
+      "weight": 30
+    },
+    // {
+    //   "link": "https://i.imgur.com/huPENVx.png",
+    //   "weight": 30
+    // },
+    {
+      "link": "https://i.imgur.com/nkhC08W.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/20Gvjhl.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/SIPWNRY.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/PfdG49k.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/Nkvog3X.jpg",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/FDFygbi.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/XnV7CzX.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/GtyMfkE.jpg",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/EYTgZNj.jpg",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/eyeU5W3.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/XGMKXrQ.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/FEltzOV.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/0IcWsho.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/pLYT6IQ.jph",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/QZeZg8D.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/IpqN4Ld.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/eI8Zbem.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/E2Fp9ee.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/10VVDQh.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/HaHw8RL.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/sHFbPUn.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/eZhe0Lp.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/WMXBJxd.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/BoZX7VR.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/Nvvo6PB.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/pSN4M0p.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/MimRofb.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/INRLCRm.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/anbyygG.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/9HgGSHA.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/4toxJ0C.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/Lz9s632.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/8cdn9jj.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/H3FPqdg.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/wex27qo.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/TLXrMpM.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/jzrBQLt.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/dNCrPlJ.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/KJCSzRO.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/Elg65lF.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/l2K3j2T.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/ecsVh0S.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/cdFJcdd.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/S4bTapM.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/0tEvmgl.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/uls2It1.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/SIPWNRY.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/ecsVh0S.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/huPENVx.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/QEudKwV.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/SIPWNRY.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/ecsVh0S.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/huPENVx.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/e7ZRWRc.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/yUEGWgz.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/1P3iUiv.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/kgrvMEf.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/aVGSE7y.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/xsduhPV.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/WyRUghI.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/flSSo3i.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/OTKHGAu.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/FKmDU6q.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/Ny1JASl.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/oey1DIq.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/AEEgHfY.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/W23uTPt.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/hfdbQYb.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/MNY3cOm.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/LhxprfC.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/1iauVM1.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/fArOvd8.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/4DrtsKj.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/18oA1kH.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/oGzHvik.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/SIPWNRY.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/E3FKp9s.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/VG1IBdc.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/M2CLbbt.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/PuUqUZQ.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/WSaETV5.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/s33aR6r.jpg",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/l4EG9TG.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/BXS3z5p.png",
+      "weight": 1
+    },
+    {
+      "link": "https://i.imgur.com/7oMExPn.jpg",
+      "weight": 1
+    }
+  ]
+}
