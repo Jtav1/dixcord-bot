cc7b8470f21301d1cd361542d989b810f1257de8
Justin Tavares <justintavares@gmail.com>
2022-10-26 21:56:29 -0400

    use local env files for testing also remove unneeded vars for new algo

--- configVars.js
+++ configVars.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs');
+require('dotenv').config();
 
 // file location for list of URLs
-const take_a_look_list_file_loc = '/data/take_a_look_list.txt';
+let take_a_look_list_file_loc = '/data/take_a_look_list.txt';
 
 // initialise values that will be exported later
-let takeALookSources = [];
-let takeALookAry = null;
+let defaultArray = [];
+let rareArray = [];
 
 // Utility function for killing program if missing setup
 const writeError = (err) => {
@@ -21,7 +22,10 @@ const writeError = (err) => {
 
 // Check if data directory mounted
 if(!fs.existsSync('/data')){
-  writeError("Data directory not mounted. Add external volume, mounted internaly at /data")
+  console.log("Data directory not mounted. Assuming you are testing locally, using dev env vars");
+  take_a_look_list_file_loc = process.env.DEV_TAKE_A_LOOK_AT_THIS_LINKS_FILE;
+
+  if(take_a_look_list_file_loc.length < 1) writeError("Data directory not mounted, and no DEV_TAKE_A_LOOK_AT_THIS_LINKS_FILE in .env")
 }
 
 // Put files in the data directory for population if one isn't already there
@@ -29,10 +33,10 @@ fs.writeFile(take_a_look_list_file_loc, '', { flag: 'wx' }, function (err) {
   if(err) {
     console.log(take_a_look_list_file_loc + " exists, skipping creation");
   } else {
-    console.log("Created /data/take_a_look_at_this.txt")
+    console.log("Created " + take_a_look_list_file_loc)
   }
   
-  console.log("Make sure you have a list of imgur links in /data/take_a_look_at_this.txt - one link per line.");
+  console.log("Make sure you have a list of imgur links in " + take_a_look_list_file_loc + " - one link per line.");
   console.log("And also put a * at the beginning of the default Riker picture e.g. *https://...");
 });
 
@@ -41,32 +45,35 @@ fs.writeFile(take_a_look_list_file_loc, '', { flag: 'wx' }, function (err) {
 /*********** CONFIRM ENVIRONMENT VARIABLES ARE SET **********/
 //INCL: token - DISCORD BOT AUTH TOKEN
 //      clientId - DISCORD BOT CLIENT ID
-//      [weight scale values] - FOR BUILDING RANDOM IMG LINK ARRAY
+//      rareFrequency - HOW OFTEN DO RARE "TAKE A LOOK AT THIS" LINKS GET RETURNED 0-1
 
 let token = process.env.DISCORD_TOKEN;
 if(token == null){
-  writeError("No token provided. Please add environment variable called DISCORD_TOKEN with bots discord auth token."); 
+  console.log("No token provided. Assuming you are testing locally, using dev env vars");
+  token = process.env.DEV_DISCORD_TOKEN;
+
+  if(token.length < 1) writeError("Discord auth token not found, and no DEV_DISCORD_TOKEN in .env") 
 }
 
 let clientId = process.env.DISCORD_CLIENT_ID;
 if(clientId == null){
-  writeError("No token provided. Please add environment variable called DISCORD_CLIENT_ID with bots discord client id."); 
+  console.log("No clientId provided. Assuming you are testing locally, using dev env vars");
+  clientId = process.env.DEV_CLIENT_ID;
+
+  if(clientId.length < 1) writeError("Discord clientId not found, and no DEV_CLIENT_ID in .env") 
 }
 
-let uncommonScale = process.env.RARITY_UNCOMMON;
-let rareScale = process.env.RARITY_RARE;
-let ultraScale = process.env.RARITY_ULTRA;
-let defaultScale = process.env.RARITY_DEFAULT;
-if(uncommonScale == null || rareScale == null ||  ultraScale == null ||  defaultImgWeight == null){
-  console.log("Did not provide RARITY_UNCOMMON, RARITY_RARE, RARITY_ULTRA, RARITY_DEFAULT env vars - defaulting to normal values.")
+let rareFreqStr = process.env.RARITY_FREQ
+let rareFrequency = parseFloat(rareFreqStr);
 
-  uncommonScale = 180;
-  rareScale = 80;
-  ultraScale = 10;
-  defaultScale = 3000;
+if(isNaN(rareFrequency) || rareFrequency > 1.0 || rareFrequency < 0.0){
+  console.log("No valid rareFreq provided. Assuming you are testing locally, using dev env vars");
+  rareFrequency = process.env.DEV_RARE_FREQ;
+
+  console.log("rareFrequency set to: " + rareFrequency);
+
+  if(isNaN(rareFrequency) || rareFrequency > 1.0 || rareFrequency < 0.0) writeError("Did not provide valid DEV_RARE_FREQ between 0 and 1, defaulting to normal value - 0.10 or 10% chance of rare link.") 
 }
-
-
 
 /*********** PROCESSING FUNCTIONS **********/
 
@@ -78,54 +85,17 @@ const processTakeImageLinks = () => {
   let lines = file.split(/\r?\n/);
 
   lines.forEach((line) => {
-    const weightScale = () => {
-      let scaleSelect = Math.floor(Math.random() * 3) + 1;
-
-      switch(scaleSelect){
-        case 1:
-          return uncommonScale;
-        case 2:
-          return rareScale;
-        case 3:
-          return ultraScale;
-        default:
-          writeError("Whoa math itself has broken somehow");
-      }
-    }
-
-    let weight = Math.floor(Math.random() * weightScale()) + 1;
-    
-    //Pull the asterisk out if it exists
-    addLine = line;
     if(line.startsWith("*")) {
-      weight = defaultScale; 
-      addLine = line.replace(/[*]/g, "");
+      defaultArray.push(line.replace(/[*]/g, "").trim());
+    } else {
+      rareArray.push(line.trim());
     }
-
-    addLine = addLine.trim();
-    if(addLine.length > 0) takeALookSources.push({ link: addLine, weight: weight });
 
   })
 
-  cacheSources();
-}
+  console.log("Default Array contains " + defaultArray.length + " links");
+  console.log("Rare Array contains " + rareArray.length + " links");
 
-// cacheSources()
-//  Build a full array on initialization to choose response links from
-//  each link will be in here its [weight] times
-const cacheSources = () => {
-
-  if(takeALookAry == null) {
-    linkTbl = [];
-
-    takeALookSources.forEach((img) => {
-      for(i = 0; i < img.weight; i++){
-        linkTbl.push(img.link);
-      }
-    });
-  
-    takeALookAry = linkTbl;
-  }
 }
 
 //Kick off image array building process
@@ -134,9 +104,7 @@ processTakeImageLinks();
 module.exports = {
   token,
   clientId,
-  takeALookAry,
-  takeALookSources,
-  uncommonScale,
-  rareScale,
-  ultraScale
+  defaultArray,
+  rareArray,
+  rareFrequency
 };
