d399e57f0fa42ec850c8f05b21c714123199dcd1
Justin Tavares <justintavares@gmail.com>
2024-01-09 04:02:55 -0500

    initial commit of function to log chat messages

---
+++ Logging/dataLog.js
@@ -0,0 +1,30 @@
+const { logFile, log_filter_list_loc, filterWordArray } = require('../configVars.js');
+
+
+
+module.exports = {
+	cleanLog: function(message) {
+    const userIdRegex = /<@\d{18}> /;
+    const addressRegex = /\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\./
+    const phoneNumRegex = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/
+    const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+/
+    const spaceRegex = /\s+/g;
+
+    let logMsg = message.content
+      .replace(userIdRegex, '')
+      .replace(addressRegex, '')
+      .replace(phoneNumRegex, '')
+      .replace(emailRegex, '');
+
+    filterWordArray.forEach((keyword) => {
+      logMsg = logMsg.replace(keyword, '');
+    })
+
+    logMsg = logMsg.replace(spaceRegex, ' ').trim();
+
+    console.log("LOGGING: " + logMsg);
+    // log here
+
+  }
+
+}
