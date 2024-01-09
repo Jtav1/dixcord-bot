85a20c58d3df621da456660d42e8b2a043c5fa2d
Justin Tavares <justintavares@gmail.com>
2024-01-09 04:03:21 -0500

    add twitter link fixer and restructure responses to only send one

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,7 @@
 const { clientId, defaultArray, rareArray, rareFrequency, positiveArray, negativeArray, neutralArray } = require('../configVars.js');
 
+var dataLog = require('../Logging/dataLog.js');
+
 // var Sentiment = require('sentiment');
 // var sentiment = new Sentiment();
 
@@ -7,9 +9,10 @@ let limit = 0;
 let lastReplyTimestamp = null;
 let ms = (Math.random() * 60000) + 60000; // CUTTING EDGE AI ANTI EXPLOIT TECHNOLOGY
 // let ms = 10000;
+
 let configuredLimit = 2;
 
-module.exports =  {
+module.exports = {
 	name: 'messageCreate',
 	execute(message) {
 
@@ -19,7 +22,6 @@ module.exports =  {
 		//	selects an image and sends a reply containing the link
 		//	if the image has a sufficient rarity, also says that
 		const takeALook = () => {
-			
 			let nowTime = Date.now();
 
 			if(lastReplyTimestamp == null || Math.floor((nowTime - lastReplyTimestamp)) >= ms){
@@ -34,13 +36,7 @@ module.exports =  {
 				lastReplyTimestamp = Date.now();
 				limit++;
 				
-				message.reply("No spam!"); // https://i.imgur.com/kAClxb0.png = spam picture url lol
-
-				return;
-
-			} else {
-				//do nothing
-				return;
+				return "No spam!"; // https://i.imgur.com/kAClxb0.png = spam picture url lol
 			}
 
 			let imgLink = "";
@@ -57,42 +53,82 @@ module.exports =  {
 			limit++;
 			lastReplyTimestamp = Date.now();
 
-			message.reply(imgLink);
+			return imgLink;
 		}
-
 
 		// fortuneTeller()
 		//	Randomly sends a fortune
 		const fortuneTeller = (rawMessage) => {
-			console.log("Fortune teller: ")
 			const processedMessage = rawMessage.replace("<@" + clientId + ">", "")
-			console.log(rawMessage + " => " + processedMessage);
+			//console.log(rawMessage + " => " + processedMessage);
+
 			//var result = sentiment.analyze(processedMessage);
 
 			const combinedResponses = positiveArray.concat(negativeArray, neutralArray);
+
+
 			console.log(combinedResponses.length);
-			message.reply(combinedResponses[Math.floor(Math.random() * combinedResponses.length)]);
+			return(combinedResponses[Math.floor(Math.random() * combinedResponses.length)]);
+		}
+
+		// twitterFixer(str, bool)
+		//  reply with a vx twitter link if a non-vx twitter link is posted
+		//	urlType = true for x.com false for twitter.com
+		const twitterFixer =  (url, urlType) => {
+			if(urlType){
+				return "fixed it: " + url.replace('https://x.com/', 'https://vxtwitter.com/');
+			} else {
+				return "fixed it: " + url.replace('https://twitter.com/', 'https://vxtwitter.com/');
+			}
 		}
 
 		//******* INCOMING MESSAGE PROCESSING *******//
+
+		let response = "";
 			
 		if(!message.author.bot && !(message.author.id === clientId)){
-			
+
 			// List of all response functions
 			//let commandDict = {
 			//	'takealookatthis': ,
 			//	'dixbotfortune': fortuneTeller,
 			//}
+			const sentence = message.content.split(' ');
+
+			sentence.forEach(async word => {
+				if(word.startsWith('https://twitter.com/')){
+					response = twitterFixer(word, false);
+				} else if(word.startsWith('https://x.com/')){
+					response = twitterFixer(word, true);
+				}
+			})
+
+			if(response.length > 0){
+				console.log("@" + message.name);
+				message.reply(response);
+				return;
+			}
 
 			// Strip incoming message for comparison		
 			const contentStripped = message.content.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 			
 			// If the incoming message contains a response trigger phrase
 			if(contentStripped.includes("takealookatthis")){
-				takeALook();
+				response = takeALook();
 			} else if(contentStripped.startsWith(clientId) && message.content.endsWith("?")){
-				fortuneTeller(message.content.toLowerCase());
+				response = fortuneTeller(message.content.toLowerCase());
 			}
+
+			if(response.length > 0){
+				console.log("<@" + message.author.username + '>: ' + message.cleanContent);
+				message.reply(response);
+				return;
+			}
+
+			// If it wasnt a dixbot keyword, log the message for later bot training purposes
+			// dataLog.cleanLog pulls out all mentions of userID and a preset list of names
+			dataLog.cleanLog(message);
+
 		}
 	},
 };
