61fe4f01b31d4e8d6ff518f92cae62c0d3d9c978
Justin Tavares <justintavares@gmail.com>
2024-01-10 03:52:11 -0500

    finish clean logger

--- Logging/dataLog.js
+++ Logging/dataLog.js
@@ -1,36 +1,44 @@
 const { logFile, log_filter_list_loc, filterWordArray } = require('../configVars.js');
-
-
+const fs = require('node:fs');
 
 module.exports = {
 	cleanLog: function(message) {
-    const userIdRegex = /<@\d{18}> /;
-    const addressRegex = /\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\./
-    const phoneNumRegex = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/
-    const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+/
+    const userIdRegex = /<@\d+>/g;
+    const groupIdRegex = /<@&\d+>/g;
+    const addressRegex = /\d{1,5}\s{1}\w+\s{1}\w+\s\w+/g;
+    const phoneNumRegex = /(\([0-9]{3}\)|[0-9]{3})( |-)[0-9]{3}-[0-9]{4}/g;
+    const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+/g;
+    const urlRegex = /(http:\/\/|https:\/\/)[^\s]+/g;
+
     const spaceRegex = /\s+/g;
-    //todo remove attachments and URLs
 
     //todo add logfile encryption
 
-
     let logMsg = message.content
       .replace(userIdRegex, '')
-      .replace(addressRegex, '')
+      .replace(groupIdRegex, '')
       .replace(phoneNumRegex, '')
-      .replace(emailRegex, '');
+      .replace(emailRegex, '')
+      .replace(urlRegex, '')
+      .replace(addressRegex, '');
 
-    filterWordArray.forEach((keyword) => {
 
-      //todo split, compare ignore case, rejoin without
-      logMsg = logMsg.replace(keyword, '');
+    let tmpAry = logMsg.split(' ');
+
+
+    let msgAry = tmpAry.filter((word) =>{
+      return !filterWordArray.includes(word);
     })
 
-    logMsg = logMsg.replace(spaceRegex, ' ').trim();
+    let cleanMsg = msgAry.join(' ').replace(spaceRegex, ' ').trim();
 
-    console.log("LOGGING: " + logMsg);
-    //todo log here
-
+    if(cleanMsg.length > 0){
+      fs.appendFile(logFile, "\n"+cleanMsg, (err) => { 
+        if (err) { 
+          console.log("Error writing to chatlog file " + logFile + ": " + err); 
+        } 
+      });
+    } 
   }
 
 }
