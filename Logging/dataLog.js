238baca20e161cf964eda442783c24476842b5bc
Justin Tavares <justin@jtav.me>
2024-08-08 03:04:56 -0400

    prettier

--- Logging/dataLog.js
+++ Logging/dataLog.js
@@ -1,4 +1,4 @@
-import { logFile, filterWordArray, mysqlHost, mysqlPort, mysqlUser, mysqlPw, mysqlDb, isDev} from '../configVars.js';
+import { logFile, filterWordArray, mysqlHost, mysqlPort, mysqlUser, mysqlPw, mysqlDb, isDev } from '../configVars.js';
 import fs from 'node:fs';
 
 import mysql from 'mysql2';
@@ -33,14 +33,14 @@ const emojiTblName = (isDev ? "dev_emoji_frequency" : "emoji_frequency");
 
 const tblCreateQuery = mysql.format(
   "CREATE TABLE IF NOT EXISTS " + emojiTblName +
-    " (emoid VARCHAR(255) NOT NULL, " + 
-    "emoji VARCHAR(255) NOT NULL, " +
-    "frequency INT NOT NULL, " +
-    "animated BOOLEAN, " +
-    "type VARCHAR(50) NOT NULL, " +
-    "PRIMARY KEY (emoid))");
+  " (emoid VARCHAR(255) NOT NULL, " +
+  "emoji VARCHAR(255) NOT NULL, " +
+  "frequency INT NOT NULL, " +
+  "animated BOOLEAN, " +
+  "type VARCHAR(50) NOT NULL, " +
+  "PRIMARY KEY (emoid))");
 
-    con.query(tblCreateQuery);
+con.query(tblCreateQuery);
 console.log('-- Emoji table tracking created');
 
 
@@ -48,7 +48,7 @@ const initializeEmojisList = (emojiObjectList) => {
   let emojiArray = [];
 
   emojiObjectList.forEach((e) => {
-    emojiArray.push(([`${e.id}`, `${e.name}` , 0, e.animated, `${e.type}`]));
+    emojiArray.push(([`${e.id}`, `${e.name}`, 0, e.animated, `${e.type}`]));
   });
 
   con.query("DELETE FROM " + emojiTblName + " WHERE frequency = 0 and type = 'emoji'");
@@ -64,8 +64,8 @@ const initializeEmojisList = (emojiObjectList) => {
 
 const countEmoji = (emoji) => {
   let emoCleaned = emoji.replace("<", "").replace(">", "").split(":").slice(1);
-  
-  if(emoCleaned.length == 2){
+
+  if (emoCleaned.length == 2) {
     const emoIncrementQry = mysql.format(
       "UPDATE " + emojiTblName + " SET frequency = frequency + 1 WHERE emoji = ? AND emoid = ?",
       [emoCleaned[0], emoCleaned[1]]
@@ -78,7 +78,7 @@ const countEmoji = (emoji) => {
 
 const getTopEmoji = (number) => {
 
- // this does not exist
+  // this does not exist
 
 }
 
@@ -106,19 +106,19 @@ const cleanLog = (message) => {
   let tmpAry = logMsg.split(' ');
 
 
-  let msgAry = tmpAry.filter((word) =>{
+  let msgAry = tmpAry.filter((word) => {
     return !filterWordArray.includes(word);
   })
 
   let cleanMsg = msgAry.join(' ').replace(spaceRegex, ' ').trim();
 
-  if(cleanMsg.length > 0){
-    fs.appendFile(logFile, cleanMsg+"\n", (err) => { 
-      if (err) { 
-        console.log("Error writing to chatlog file " + logFile + ": " + err); 
-      } 
+  if (cleanMsg.length > 0) {
+    fs.appendFile(logFile, cleanMsg + "\n", (err) => {
+      if (err) {
+        console.log("Error writing to chatlog file " + logFile + ": " + err);
+      }
     });
-  } 
+  }
 }
 
-export default { cleanLog, countEmoji, initializeEmojisList, getTopEmoji};
+export default { cleanLog, countEmoji, initializeEmojisList, getTopEmoji };
