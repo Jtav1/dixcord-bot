ca8db8ea3215c687675a6783e8916b9f99876485
Justin Tavares <justin@jtav.me>
2024-09-30 13:38:04 -0400

    remove debug log statements

--- commands/utilities/emoji-leaderboards.js
+++ commands/utilities/emoji-leaderboards.js
@@ -10,20 +10,13 @@ const data = new SlashCommandBuilder()
 		.setDescription('Top 5 Most Used Emojis in Dixcord');
 
 const execute = async (interaction) => {
-    console.log("in emoji leaderboard execute");
-
     let top5 = await dataLog.getTopEmoji(5);
-
-    console.log("top5 complete");
-    console.log(top5);
 
     let emoStr = 'Top 5 most used emojis:\n\n';
     top5.forEach((em, idx) => {
         let num = idx+1
         emoStr += '\n\t' + num + ' - ' + em.emoji + ', ' + em.frequency + (em.animated ? ' <a:' : ' <:') + em.emoji + ':' + em.emoid + '>';
     });
-
-    console.log(emoStr)
 
     const emojiEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
