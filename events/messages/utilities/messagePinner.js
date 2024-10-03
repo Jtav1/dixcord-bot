77019624f29a3451709a12cf6e20c1bd9bd8c6af
Justin Tavares <justin@jtav.me>
2024-10-03 06:10:41 -0400

    add user to pin embed

--- events/messages/utilities/messagePinner.js
+++ events/messages/utilities/messagePinner.js
@@ -1,6 +1,7 @@
 import dataLog from '../../../logging/dataLog.js';
 import { EmbedBuilder } from "discord.js";
 
+import { isDev } from '../../../configVars.js';
 
 const pinChannelId = '915462110761349201'; //TODO move this to config table in db
 
@@ -24,6 +25,7 @@ const messagePinner = async (message, pinReaction, user, client) => {
 
         const pinEmbed = new EmbedBuilder()
             .setColor(0xBC0302)
+            .setAuthor({ name: (message.author.displayName + ' (' + message.author.username + ')'), iconURL: message.author.displayAvatarURL(), url: message.url })
             .setTitle('📌 Major Pin Alert')
             .setURL(message.url)
             .setDescription(message.content)
