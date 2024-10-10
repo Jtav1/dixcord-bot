930016e3ea92e23a74c491f86435afd36dde1fc6
Justin Tavares <justin@jtav.me>
2024-10-10 14:29:33 -0400

    switch to execQuery

--- middleware/responses.js
+++ middleware/responses.js
@@ -1,56 +1,17 @@
-import {
-  logFile,
-  filterWordArray,
-  mysqlHost,
-  mysqlPort,
-  mysqlUser,
-  mysqlPw,
-  mysqlDb,
-  isDev,
-} from "../configVars.js";
-import fs from "node:fs";
+import { logFile, filterWordArray, isDev } from "../configVars.js";
+
+import { execQuery } from "../database/queryRunner.js";
 
 import mysql from "mysql2";
 
-var con = mysql.createPool({
-  host: mysqlHost,
-  port: mysqlPort,
-  user: mysqlUser,
-  password: mysqlPw,
-  database: mysqlDb,
-});
-
-con.on("connection", function (connection) {
-  console.log("DB Connection established");
-
-  connection.on("error", function (err) {
-    console.error(new Date(), "MySQL error", err.code);
-  });
-  connection.on("close", function (err) {
-    console.error(new Date(), "MySQL close", err);
-  });
-});
-
-con.addListener("error", (err) => {
-  console.log(err);
-});
-
-//async wrapper for running select queries
-async function getResults(qry) {
-  try {
-    const [rows] = await con.promise().query(qry);
-    return rows; // Return the result
-  } catch (e) {
-    console.err(e);
-    return [];
-  }
-}
-
 const getRareArray = async () => {
-  var query = con.format(
+  var query = mysql.format(
     "SELECT link FROM take_a_look_responses WHERE isdefault = 0"
   );
-  const results = await getResults(query);
+  const results = await execQuery(query);
+
+  console.log("------ rare array ------");
+  console.log(results);
 
   //map this to an array of strings
   const tempArray = results.map((row) => {
@@ -61,10 +22,13 @@ const getRareArray = async () => {
 };
 
 const getCommonArray = async () => {
-  var query = con.format(
+  var query = mysql.format(
     "SELECT link FROM take_a_look_responses WHERE isdefault = 1"
   );
-  const results = await getResults(query);
+  const results = await execQuery(query);
+
+  console.log("------ common array ------");
+  console.log(results);
 
   //map this to an array of strings
   const tempArray = results.map((row) => {
@@ -77,10 +41,9 @@ const getCommonArray = async () => {
 const getAllTakeALookLinks = async () => {
   const tmpArray = [];
 
-  var query = con.format("SELECT * FROM take_a_look_responses");
-  const results = await getResults(query);
+  var getAllQry = mysql.format("SELECT * FROM take_a_look_responses");
 
-  return results;
+  return await execQuery(query);
 };
 
 const insertTakeALookLink = async (link, isdefault) => {
@@ -89,14 +52,7 @@ const insertTakeALookLink = async (link, isdefault) => {
     [link, isdefault]
   );
 
-  con
-    .query(linkInsertQry)
-    .then(() => {
-      return true;
-    })
-    .catch((err) => {
-      return false;
-    });
+  return await execQuery(linkInsertQry);
 };
 
 const incrementTakeALookLink = async (link) => {
@@ -105,8 +61,7 @@ const incrementTakeALookLink = async (link) => {
     [link]
   );
 
-  //TODO figure out how to return success/failure here
-  con.query(linkInsertQry);
+  return await execQuery(linkInsertQry);
 };
 
 export {
