e627c0c05dc57f218b54460544d344f864d50a9a
Justin Tavares <justin@jtav.me>
2024-10-16 16:15:48 -0400

    add config var object template (initial revision)

---
+++ middleware/classes/configObject.js
@@ -0,0 +1,12 @@
+class Config {
+  constructor(token, clientId, guildId) {
+    this.token = token;
+    this.clientId = clientId;
+    this.guildId = guildId;
+    this.rareFreq = rareFreq;
+    this.logFile = logFile;
+    this.twitterFixer = twitterFixer;
+  }
+}
+
+export default Config;
