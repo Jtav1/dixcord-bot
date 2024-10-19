7208bc532e7f3d91f9391f2e7090bb6ab2003488
Justin Tavares <justin@jtav.me>
2024-10-19 01:38:32 -0400

    add config middleware

---
+++ middleware/configurations.js
@@ -0,0 +1,28 @@
+import { execQuery } from "../database/queryRunner.js";
+
+import mysql from "mysql2";
+
+export const getAllConfigurations = async () => {
+  var query = mysql.format("SELECT * FROM configurations");
+  return await execQuery(query);
+};
+
+export const addConfiguration = async (config, value) => {
+  const query = mysql.format(
+    "INSERT IGNORE INTO configurations (config, value) VALUES ?",
+    [[[config, value]]]
+  );
+
+  console.log(query);
+
+  return await execQuery(query);
+};
+
+export const updateConfigurationByName = async (config, value) => {
+  const query = mysql.format(
+    "UPDATE configurations SET VALUE = ? WHERE config = ?",
+    [value, config]
+  );
+
+  return await execQuery(query);
+};
