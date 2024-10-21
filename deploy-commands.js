15c8e5b807d85fdbf0c6dd689fe776e9637f2206
Justin Tavares <justin@jtav.me>
2024-10-21 01:53:42 -0400

    cleanup/formatting

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,42 +1,50 @@
-import { REST, Routes } from 'discord.js';
-import { clientId, guildId, token } from './configVars.js';
-import fs from 'node:fs';
-import path from 'node:path';
+import { REST, Routes } from "discord.js";
+import { clientId, guildId, token } from "./configVars.js";
+import fs from "node:fs";
+import path from "node:path";
 
 const commands = [];
-const foldersPath = path.join(process.cwd(), 'commands');
+const foldersPath = path.join(process.cwd(), "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = await import(filePath);
-		if ('data' in command && 'execute' in command) {
-			console.log("Found command, adding...");
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(filePath);
+    if ("data" in command && "execute" in command) {
+      console.log("Found command, adding...");
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
 
-		// if(command.data && command.execute){
-		// 	console.log("use this other format 0000000");
-		// }
-	}
+    // if(command.data && command.execute){
+    // 	console.log("use this other format 0000000");
+    // }
+  }
 }
 
 const rest = new REST().setToken(token);
 
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
 })();
