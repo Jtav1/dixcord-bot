5532d7eaf99981bae22be28c890f5280b6416d39
Justin Tavares <justin@jtav.me>
2026-03-04 13:58:42 -0500

    Remove extraneous function calls

--- discord-bot/events/messages/responses/takeALook.js
+++
@@ -1,9 +0,0 @@
-import { takeALook as getTakeALookResponse } from "../../../api/responses.js";
-
-/**
- * Take-a-look response: random image URL, "No spam!", or empty if over limit.
- * All logic (rate limit, rare/common, increment) is in the webapi.
- */
-export const takeALook = async () => {
-  return await getTakeALookResponse();
-};
